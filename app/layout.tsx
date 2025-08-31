fc88d560145a0c7413b2091ee6d20314ab254fe6
Jare9716 <jare9716@gmail.com>
2025-08-31 14:21:37 -0500

    fixing metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,13 +37,13 @@ export const metadata: Metadata = {
 		siteName: "Oblicuo",
 		locale: "en_AU",
 		type: "website",
-		images: [
-			{
-				url: "https://oblicuo.vercel.app/opengraph-image.png",
-				width: 1200,
-				height: 630,
-			},
-		],
+		// images: [
+		// 	{
+		// 		url: "https://oblicuo.vercel.app/opengraph-image.png",
+		// 		width: 1200,
+		// 		height: 630,
+		// 	},
+		// ],
 	},
 	twitter: {
 		card: "summary_large_image",
@@ -51,11 +51,11 @@ export const metadata: Metadata = {
 		description:
 			"Luxury, custom cabinetry and furniture for modern Australian homes.",
 		creator: "@Oblicuo",
-		images: ["https://oblicuo.vercel.app/opengraph-image.png"],
+		// images: ["https://oblicuo.vercel.app/opengraph-image.png"],
 	},
-	icons: {
-		icon: "/favicon.ico",
-	},
+	// icons: {
+	// 	icon: "/favicon.ico",
+	// },
 };
 
 export default function RootLayout({
